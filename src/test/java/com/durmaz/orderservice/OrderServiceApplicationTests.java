446a5306279bc40d176817f3ca23f230b4e7b6d3
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-05-17 12:09:29 +0300

    start order-service project

---
+++ src/test/java/com/durmaz/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durmaz.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
