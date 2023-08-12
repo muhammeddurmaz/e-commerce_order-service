f2bc3d6163aeb2e5d05c0c29b85dbc093919c6f8
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-08-12 10:24:37 +0300

    OrderExists test method added

---
+++ src/test/java/com/durmaz/orderservice/service/OrderItemServiceTest.java
@@ -0,0 +1,13 @@
+package com.durmaz.orderservice.service;
+
+import org.mockito.*;
+
+public class OrderItemServiceTest {
+
+
+    @InjectMocks
+    private OrderItemService orderItemService;
+
+
+
+}
